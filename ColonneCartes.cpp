1e9ad813033fd5a5d5303e502c0c6f253ad9c2a7
Lord Catzorz <LordCatzorz@users.noreply.github.com>
2015-10-17 15:57:24 -0400

    Transfert doccumentation du .h au .cpp pour ColonneCartes

--- ColonneCartes.cpp
+++ ColonneCartes.cpp
@@ -8,6 +8,7 @@
 #include "ColonneCartes.h"
 
 /**
+* @brief Constructeur d'une colonne de carte.
 * Cette méthode crée une coquille pour une ColonneCarte.
 * Elle initialise son vecteur de carte comme un vecteur vide.
 */
@@ -17,6 +18,8 @@ ColonneCartes::ColonneCartes()
 }
 
 /**
+* @brief Initialise ColonneCarte avec les Cartes contenus dans le vecteur en paramètre.
+* @param p_listeCarte Les Cartes avec lesquels initialiser la ColonneCarte
 * Cette méthode vide le vecteur actuel de carte de la colonne carte pour
 * le remplacer par le vecteur en paramètre. 
 * Il va aussi remettre le nombre de carte visible à 1.
@@ -32,6 +35,8 @@ void ColonneCartes::initColonneCartes (std::vector<Carte>& p_listeCartes)
 }
 
 /**
+* @brief Cette méthode ajoute une Carte à la fin de colonne carte.
+* @param p_carte La Carte à ajouter à la fin de la colonne.
 * Cette méthode s'attend à ajouter une carte qui lui est possible d'ajouter selon les règles du Klondike.
 * Si la carte à ajouter n'est pas valide, une erreur de précondition est lancée.
 * Cette méthode ajoute à la carte à la suite du vecteur et incrémente le nombre de carte visible.
@@ -47,6 +52,9 @@ void ColonneCartes::ajoute (const Carte&  p_carte)
 }
 
 /**
+* @brief Cette méthode prend des cartes de la colonne et les déplaces à la suite d'une autre colonne.
+* @param p_destination La ColonneCarte sur laquelle ajouter les cartes déplacés.
+* @param p_nombreCartes La quantité de carte à déplacer.
 * Cette méthode s'attend à avoir comme instruction d'effectuer un coup valide selon les règles du Klondike.
 * Si ce n'est pas le cas, une erreur de précondition est lancée.
 * Cette méthode boucle au travers de l'ensemble des cartes à déplacer et les ajoutes 1 à 1 sur la ColonneCarte destinations.
@@ -72,6 +80,7 @@ void ColonneCartes::deplacePaquet (ColonneCartes& p_destination, int p_nombreCar
 }
 
 /**
+* @brief Cette méthode supprime la dernière Carte qui est sur le dessus de la ColonneCarte
 * Cette méthode s'attend à être appelée lorsqu'il y a encore au moins un carte.
 * Si la colonne est vide, une erreur de précondition est lancée.
 * Elle supprime la dernière carte, puis décrémente le nombre de carte visible.
@@ -83,12 +92,20 @@ void ColonneCartes::supprimeDerniereCarte ()
 	this->changerNombreCarteVisible(-1);
 }
 
+/**
+* @brief Cette méthode retourne le nombre de carte qui sont visible sur la colonne.
+* @return Le nombre de carte visible en constante.
+*/
 const int ColonneCartes::reqNbCartesVisibles () const
 {
 	return this->m_nbCartesVisibles;
 }
 
 /**
+* @brief Surcharge de l'opérateur << qui fait l'affichage d'une ColonneCarte
+* @param p_sortie Une référence vers l'objet ostream de l'opérateur <<
+* @param p_carte Une référence constante vers une ColonneCarte
+* @return Un ostream représentant la ColonneCarte
 * Cette méthode affiche une colonne selon ces règles:
 *	1. Si la pile est vide, affiché une X.
 *	2. Pour chaque carte cachée, afficher un ?.
@@ -121,6 +138,8 @@ std::ostream& operator<< (std::ostream& p_sortie, const ColonneCartes& p_colonne
 }
 
 /**
+* @brief Cette méthode change le nombre de carte selon un terme donné en paramètre.
+* @param p_diffenrenceNbCarte La différence de carte à effectuer
 * Après avoir ajuster le nombre de carte visible selon la quantité donnée en paramètre,
 * la méthode vérifie si le nombre de carte est inférieure à 1. Si c'est le cas, elle le remet à 1.
 */
@@ -133,12 +152,18 @@ void ColonneCartes::changerNombreCarteVisible(const int p_differenceNbCarte)
 	}
 }
 
+/**
+* @brief Cette fonction retourne les Cartes composant la ColonneCarte
+* @return Une référence constante d'un vecteur contant les Cartes de la ColonneCarte.
+*/
 const std::vector<Carte>& ColonneCartes::reqLesCartes() const
 {
 	return this->m_lesCartes;
 }
 
 /**
+* @brief Cette méthode retourne la carte qui est sur le dessus de la ColonneCarte
+* @return Une référence constante vers la Carte qui est sur le dessus de la ColonneCarte.
 * Cette méthode s'attend à être appelé seulement si la colone n'est pas vide.
 * Si elle est vide, une erreur de précondition est lancée.
 * Elle retourne la dernière carte du vecteur.
@@ -150,6 +175,9 @@ Carte& ColonneCartes::reqCarteDessus()
 }
 
 /**
+* @brief Cette méthode retourne la carte situé à une position donnée du début du paquet.
+* @param p_position La position à partir du début de la Carte désirée. Le dessus = 1.
+* @return Une référence constante vers la Carte situé à une position donnée de la ColonneCarte
 * Cette méthode s'attend à être appelé seulement si la colone n'est pas vide.
 * Elle s'attend aussi à ce que la position demandée soit une carte visible.
 * Si ce n'est pas le cas, une erreur de précondition est lancée.
@@ -162,6 +190,8 @@ Carte& ColonneCartes::reqCartePosition(const int p_position)
 }
 
 /**
+* @brief Cette méthode retourne la carte qui est sur le dessus de la ColonneCarte
+* @return Une référence vers la Carte qui est sur le dessus de la ColonneCarte.
 * Cette méthode s'attend à être appelé seulement si la colone n'est pas vide.
 * Si elle est vide, une erreur de précondition est lancée.
 * Elle retourne la dernière carte du vecteur.
@@ -173,6 +203,9 @@ const Carte& ColonneCartes::reqCarteDessus() const
 }
 
 /**
+* @brief Cette méthode retourne la carte situé à une position donnée du début du paquet.
+* @param p_position La position à partir du début de la Carte désirée. Le dessus = 1.
+* @return Une référence vers la Carte situé à une position donnée de la ColonneCarte
 * Cette méthode s'attend à être appelé seulement si la colone n'est pas vide.
 * Elle s'attend aussi à ce que la position demandée soit une carte visible.
 * Si ce n'est pas le cas, une erreur de précondition est lancée.
@@ -184,7 +217,10 @@ const Carte& ColonneCartes::reqCartePosition(const int p_position) const
 	return *(this->m_lesCartes.end() - p_position);
 }
 
-
+/**
+* @brief Cette méthode indique si la ColonneCarte n'a aucune Carte.
+* @return Vrai si aucune Carte n'est présent dans la ColonneCarte, sinon faux.
+*/
 const bool ColonneCartes::estVide() const
 {
 	return this->m_lesCartes.empty();
--- ColonneCartes.h
+++ ColonneCartes.h
@@ -15,94 +15,30 @@
 class ColonneCartes
 {
 public:
-	/**
-	* @brief Constructeur d'une colonne de carte.
-	*/
 	ColonneCartes();
 
-	/**
-	* @brief Initialise ColonneCarte avec les Cartes contenus dans le vecteur en paramètre.
-	* @param p_listeCarte Les Cartes avec lesquels initialiser la ColonneCarte
-	*/
 	void initColonneCartes (std::vector<Carte>& p_listeCartes);
 
-	/**
-	* @brief Cette méthode ajoute une Carte à la fin de colonne carte.
-	* @param p_carte La Carte à ajouter à la fin de la colonne.
-	*/
 	void ajoute (const Carte& p_carte);
-
-	/**
-	* @brief Cette méthode prend des cartes de la colonne et les déplaces à la suite d'une autre colonne.
-	* @param p_destination La ColonneCarte sur laquelle ajouter les cartes déplacés.
-	* @param p_nombreCartes La quantité de carte à déplacer.
-	*/
 	void deplacePaquet (ColonneCartes& p_destination, int p_nombreCartes);
-
-	/**
-	* @brief Cette méthode supprime la dernière Carte qui est sur le dessus de la ColonneCarte
-	*/
 	void supprimeDerniereCarte ();
 
-	/**
-	* @brief Cette méthode retourne le nombre de carte qui sont visible sur la colonne.
-	* @return Le nombre de carte visible en constante.
-	*/
 	const int reqNbCartesVisibles () const;
 
-	/**
-	* @brief Surcharge de l'opérateur << qui fait l'affichage d'une ColonneCarte
-	* @param p_sortie Une référence vers l'objet ostream de l'opérateur <<
-	* @param p_carte Une référence constante vers une ColonneCarte
-	* @return Un ostream représentant la ColonneCarte
-	*/
 	friend std::ostream& operator<< (std::ostream& p_sortie, const ColonneCartes& p_colonneCartes);
 
-	/**
-	* @brief Cette fonction retourne les Cartes composant la ColonneCarte
-	* @return Une référence constante d'un vecteur contant les Cartes de la ColonneCarte.
-	*/
 	const std::vector<Carte>& reqLesCartes() const;
-
-	/**
-	* @brief Cette méthode retourne la carte qui est sur le dessus de la ColonneCarte
-	* @return Une référence constante vers la Carte qui est sur le dessus de la ColonneCarte.
-	*/
 	const Carte& reqCarteDessus() const;
-
-	/**
-	* @brief Cette méthode retourne la carte situé à une position donnée du début du paquet.
-	* @param p_position La position à partir du début de la Carte désirée. Le dessus = 1.
-	* @return Une référence constante vers la Carte situé à une position donnée de la ColonneCarte
-	*/
 	const Carte& reqCartePosition(const int p_position) const;
 
-	/**
-	* @brief Cette méthode indique si la ColonneCarte n'a aucune Carte.
-	* @return Vrai si aucune Carte n'est présent dans la ColonneCarte, sinon faux.
-	*/
 	const bool estVide() const;
 private:
 	int m_nbCartesVisibles;
 	std::vector<Carte> m_lesCartes;
-	
-	/**
-	* @brief Cette méthode retourne la carte qui est sur le dessus de la ColonneCarte
-	* @return Une référence vers la Carte qui est sur le dessus de la ColonneCarte.
-	*/
-	Carte& reqCarteDessus();
 
-	/**
-	* @brief Cette méthode retourne la carte situé à une position donnée du début du paquet.
-	* @param p_position La position à partir du début de la Carte désirée. Le dessus = 1.
-	* @return Une référence vers la Carte situé à une position donnée de la ColonneCarte
-	*/
+	Carte& reqCarteDessus();
 	Carte& reqCartePosition(const int p_position);
 
-	/**
-	* @brief Cette méthode change le nombre de carte selon un terme donné en paramètre.
-	* @param p_diffenrenceNbCarte La différence de carte à effectuer
-	*/
 	void changerNombreCarteVisible(const int p_differenceNbCarte);
 };
 
